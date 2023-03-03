4a2e6e70ed6492b260b369aaa883cc27de745afc
notsumez <adahorapirola@gmail.com>
2023-03-03 16:22:17 -0300

    Inserts do banco

---
+++ database/inserts.sql
@@ -0,0 +1,335 @@
+insert into funcionario
+VALUES('1','daniel','2004/10/07','145.326.268-23','102.658','596.987','90.156.984-8','enfermeiro','realizar triagens, preencher prontuários, oferecer os primeiros atendimentos, coletar exames, administrar medicamentos e monitorar o estado de saúde dos pacientes.','noite','3300,00','2020/02/01','null');
+
+insert into funcionario
+VALUES('2','andrey','2000/03/10','359.742.031-98','032.987','158.387','88.032.603-7','auxiliar enfermagem','realiza a coleta de sangue para exames, faz curativos simples, administra remédios, ajuda na higiene dos pacientes, entre outras funções.','manha','2429,00','2020/02/01','null');
+
+insert into funcionario
+VALUES('3','felipe','2001/11/03','031.084.871-32','951.311','014.741','03.156.741-9','enfermeiro','realizar triagens, preencher prontuários, oferecer os primeiros atendimentos, coletar exames, administrar medicamentos e monitorar o estado de saúde dos pacientes.','manha','3136,00','2020/01/09','null');
+
+insert into funcionario
+VALUES('4','joao','1999/11/25','953.148.348-71','731.871','348.321','78.136.468-1','Cirurgião geral','especialista treinado para o manejo cirúrgico das afecções mais diversas do organismo como um todo, resolvendo todas as questões de atendimento primário e secundário, com ênfase no trauma, urgência e emergência além da captação de órgãos..','tarde','8461,00','2010/01/09','2019/11/11');
+
+insert into funcionario
+VALUES('5','mario','1998/06/05','841.168.031-94','324.014','494.684','34.887.341-5','clínico gera','responsável por diagnosticar doenças, desde as mais simples até as mais complexas.','manha','8461,00','2017/01/09','null');
+
+insert into funcionario
+VALUES('6','daniel','2000/10/07','097.631.712-22','568.445','911.200','52.641.785-9','clínico gera','responsável por diagnosticar doenças, desde as mais simples até as mais complexas.','tarde','8461,00','2017/01/09','null');
+
+insert into funcionario
+VALUES('7','maria','1998/06/05','257.987.099-97','784.565','988.655','98.795.521-7','clínico gera','responsável por diagnosticar doenças, desde as mais simples até as mais complexas.','noite','8461,00','2017/01/09','null');
+
+
+
+
+insert into tel_func
+VALUES('1','(11)93258-0384','1');
+
+insert into tel_func
+VALUES('2','(11)93146-2447','2');
+
+insert into tel_func
+VALUES('3','(11)92301-5168','3');
+
+insert into tel_func
+VALUES('4','(11)90164-9158','4');
+
+insert into tel_func
+VALUES('5','(11)91083-3058','5');
+
+insert into tel_func
+VALUES('6','(11)91354-9787','6');
+
+insert into tel_func
+VALUES('7','(11)92207-1203','7');
+
+
+insert into email_func
+VALUES('1','daniel22@gmail.com','1');
+
+insert into email_func
+VALUES('2','andrey22@gmail.com','2');
+
+insert into email_func
+VALUES('3','felipe22@gmail.com','3');
+
+insert into email_func
+VALUES('4','joao22@gmail.com','4');
+
+insert into email_func
+VALUES('5','mario22@gmail.com','5');
+
+insert into email_func
+VALUES('6','daniel44@gmail.com','6');
+
+insert into email_func
+VALUES('7','maria22@gmail.com','7');
+
+
+insert into end_func
+VALUES('1','rua valinhos','88','sao paulo','sp','03869-681','1');
+
+insert into end_func
+VALUES('2','rua valinhos','90','sao paulo','sp','03869-681','2');
+
+insert into end_func
+VALUES('3','rua melao','649','sao paulo','sp','35589-150','3');
+
+insert into end_func
+VALUES('4','rua melao','711','sao paulo','sp','35589-150','4');
+
+insert into end_func
+VALUES('5','rua cachoeira','71','sao paulo','sp','03546-698','5');
+
+insert into end_func
+VALUES('6','rua feira','8879','sao paulo','sp','89407-530','6');
+
+insert into end_func
+VALUES('7','rua alexandre','443','sao paulo','sp','22050-120','7');
+
+
+
+insert into paciente
+VALUES('1','Joao','2003/10/01','182.178.168-42','17.170.191-2','898.0034.0054.0716');
+
+insert into paciente
+VALUES('2','Joana','1987/08/05','982.268.938-97','17.170.191-2','867.1134.9954.0912');
+
+insert into paciente
+VALUES('3','Laura','1999/03/06','382.563.319-89','90.976.171-5','999.9824.0088.0512');
+
+insert into paciente
+VALUES('4','Paulo','1981/06/09','971.782.658-99','90.976.171-5','258.8765.9948.0512');
+
+insert into paciente
+VALUES('5','Ribamar','1995/03/10','871.653.765-86','73.886.190-7','299.8865.9766.4303');
+
+insert into paciente
+VALUES('6','Fred','1987/07/11','997.113.187-77','64.143.901-9','199.6665.1543.6863');
+
+insert into paciente
+VALUES('7','Viviane','1997/10/12','765.965.738-12','97.116.186-0','399.7665.1126.8649');
+
+insert into paciente
+VALUES('8','Vagner','1981/26/02','508.951.268-00','57.570.171-7','999.8765.5043.1813');
+
+insert into paciente
+VALUES('9','Eurico','1978/08/10','907.453.178-99','15.243.190-9','297.7735.7376.1981');
+
+insert into paciente
+VALUES('10','Rodrygo','1980/05/08','899.845.145-55','17.130.187-5','127.8873.634.3522'); 
+
+
+
+insert into tel_paciente
+VALUES('1','(11)93047-3015','1');
+
+insert into tel_paciente
+VALUES('2','(11)93075-6332','2');
+
+insert into tel_paciente
+VALUES('3','(11)95504-0024','3');
+
+insert into tel_paciente
+VALUES('4','(11)93884-0331','4');
+
+insert into tel_paciente
+VALUES('5','(11)93001-0015','5');
+
+insert into tel_paciente
+VALUES('6','(11)92002-3228','6');
+
+insert into tel_paciente
+VALUES('7','(11)92038-1158','7');
+
+insert into tel_paciente
+VALUES('8','(11)93804-0368','8');
+
+insert into tel_paciente
+VALUES('9','(11)93202-8741','9');
+
+insert into tel_paciente
+VALUES('10','(11)93873-3214','10');
+
+
+
+
+insert into end_paciente
+VALUES('1','av. itaquera','36','sao paulo','sp','15841-188','1');
+
+insert into end_paciente
+VALUES('2','rua marinho','11','sao paulo','sp','31484-021','2');
+
+insert into end_paciente
+VALUES('3','rua carmo','1130','sao paulo','sp','36741-687','3');
+
+insert into end_paciente
+VALUES('4','rua alexandre','685','sao paulo','sp','38413-017','4');
+
+insert into end_paciente
+VALUES('5','rua america','66874','sao paulo','sp','15895-135','5');
+
+insert into end_paciente
+VALUES('6','rua trovao','637','sao paulo','sp','07051-225','6');
+
+insert into end_paciente
+VALUES('7','rua miguel','2','sao paulo','sp','22354-786','7');
+
+insert into end_paciente
+VALUES('8','rua tatuape','3225','sao paulo','sp','10841-368','8');
+
+insert into end_paciente
+VALUES('9','rua tres ilhas','1166','sao paulo','sp','31568-315','9');
+
+insert into end_paciente
+VALUES('10','rua melancia','687','sao paulo','sp','32156-122','10');
+
+
+
+insert into email_paciente
+VALUES('1','joao23@gmail.com','1');
+
+insert into email_paciente
+VALUES('2','joana22@gmail.com','2');
+
+insert into email_paciente
+VALUES('3','laura22@gmail.com','3');
+
+insert into email_paciente
+VALUES('4','paulo22@gmail.com','4');
+
+insert into email_paciente
+VALUES('5','ribamar22@gmail.com','5');
+
+insert into email_paciente
+VALUES('6','fred22@gmail.com','6');
+
+insert into email_paciente
+VALUES('7','viviane22@gmail.com','7');
+
+insert into email_paciente
+VALUES('8','vagner22@gmail.com','8');
+
+insert into email_paciente
+VALUES('9','eurico22@gmail.com','9');
+
+insert into email_paciente
+VALUES('10','rodrygo22@gmail.com','10');
+
+
+
+insert into consulta 
+VALUES('1','2023/01/12','20:00','ferida grande com mau odor','7','9');
+
+insert into consulta 
+VALUES('2','2022/01/12','19:30','coloracao estranha na ferida','7','4');
+
+insert into consulta 
+VALUES('3','2022/01/12','19:00','inflamacao no local da ferida','7','2');
+
+insert into consulta 
+VALUES('4','2022/02/07','07:30','execo de pus na ferida','5','6');
+
+insert into consulta 
+VALUES('5','2022/02/02','15:30','reacao a o medicamento','6','8');
+
+insert into consulta 
+VALUES('6','2022/12/20','13:00','acompanhamento do tratamento','6','1');
+
+insert into consulta 
+VALUES('7','2022/12/27','17:00','retirada de ponto da ferida','6','10');
+
+insert into consulta 
+VALUES('8','2023/01/05','08:00','acompanhamento do tratamento','5','7');
+
+
+
+insert into ferida
+VALUES('1','queda de moto','2023/01/01','nenhum uso de outra medicacao','perna','inferior','alto','mau odor','esquerda','30','7','9');
+
+insert into ferida
+VALUES('2','queda de biciclete','2023/02/16','faz o uso de hormonios','costela','superior','alto','sem mau odor','esquerda','21','7','4');
+
+insert into ferida
+VALUES('3','corte com faca','2023/01/22','faz uso de insulina','mao','superior','baixo','mau odor','direita','16','7','2');
+
+insert into ferida
+VALUES('4','mordida de cachorro','2022/12/18','nenhum uso de outra medicacao','braco','superior','baixo','mau odor','direita','26','5','6');
+
+insert into ferida
+VALUES('5','queda de moto','2023/01/22','esteroides anabolizantes','braco','superior','baixo','mau odor','esquerda','10','6','8');
+
+insert into ferida
+VALUES('6','tiro','2023/01/04','nenhum uso de outra medicacao','ombro','superior','baixo','sem mau odor','direita','5','6','1');
+
+insert into ferida
+VALUES('7','acidente de bike','2023/01/15','antidepressivo','pe','inferior','alto','sem mau odor','esquerda','30','6','10');
+
+insert into ferida
+VALUES('8','queimadura','2023/09/11','antidepressivo','peito','superior','alto','sem mau odor','centro esquerda','25','5','7');
+
+
+
+insert into convenio
+VALUES('1','SulAmerica','individual','182.178.168-42','51.9563148.651.65','1');
+
+insert into convenio
+VALUES('2','SulAmerica','familiar','765.965.738-12','65.8421648.988.00','2');
+
+insert into convenio
+VALUES('3','NotreDame Intermedica','individual','871.653.765-86','01.3210255.651.30','5');
+
+
+
+
+insert into tratamento 
+VALUES ('1','possui fratura exposta','se recuperando da cirurgia','amoxicilina','prevista para 1 mes','1','1');
+
+insert into tratamento 
+VALUES ('2','queimadura severa','processo de cicatrizacao em andamento','dipirona','2 meses','3','8');
+
+insert into tratamento 
+VALUES ('3','cicatrizacao','se recuperando da cirurgia','amoxicilina','3 semanas','4','6');
+
+
+
+insert into estoque_med
+VALUES('1','dipirona','A dipirona monoidratada é um medicamento à base de dipirona, utilizado no tratamento da dor e febre.','95');
+
+insert into estoque_med
+VALUES('2','ibuprofeno','Febre e dores leves a moderadas, associadas a gripes e resfriados comuns, dor de garganta, cefaleia, dor de dente, dorsalgia, dismenorreias e mialgias.','137');
+
+insert into estoque_med
+VALUES('3','amoxicilina','Amoxicilina 500 mg é um antibiótico de largo espectro, indicado para o tratamento de infecções causadas por bactérias sensíveis à Amoxicilina, como infecções urinárias, de ouvido ou respiratórias, amigdalite, sinusite ou vaginite, em adultos e crianças.','75');
+
+insert into estoque_med
+VALUES('4','luva_cirurgica','As Luvas Cirúrgicas servem para profissionais da área da saúde utilizarem em procedimentos invasivos, que podem ser cirúrgicos ou não. ','80');
+
+insert into estoque_med
+VALUES('5','luva_procedimento','A Luva para Procedimento Não Cirúrgico Supermax é um artigo descartável, destinado para exames e procedimentos gerais, para a proteção do profissional da saúde','65');
+
+insert into estoque_med
+VALUES('6','oculos_protecao','Óculos de segurança modelo ampla-visão constituído de armação confeccionada em uma única peça de plástico rígido na cor branca.','97');
+
+
+
+
+insert into medicamento
+VALUES('1','dipirona em 8H em 8H','dipirona','2','2022/12/19','2','8','3');
+
+insert into medicamento
+VALUES('2','amoxicilina em 8H em 8H','amoxicilina','2','2023/02/19','2','1','3');
+
+insert into medicamento
+VALUES('3','dipirona em 8H em 8H','dipirona','2','2022/01/06','2','6','1');
+
+
+
+insert into Epis
+VALUES('1','4','1','4');
+
+insert into Epis
+VALUES('2','5','3','5');
+
+insert into Epis
+VALUES('3','1','7','6');
