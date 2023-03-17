a42e8fa4416ec9ae721bcb431d1c93de9016105d
notsumez <adahorapirola@gmail.com>
2023-03-17 15:57:06 -0300

    linha em baixo do header aparecendo

--- header_publico.php
+++ header_publico.php
@@ -6,36 +6,34 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/estilo.css">
-    <title>Document</title>
+    <title>Header Publico</title>
 </head>
 <body>
     <div>
-<nav class="navbar navbar-expand-lg fixed navbar fixed-top navbar-inverse" style="background-color: #38B6FF;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php#">
-        <img src="images/logo.png" alt="logo" width="100">
-    </a>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php#conscientizacao" style="color: #fff;">Conscientização</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php#dowloads" style="color: #fff;">Downloads</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php#sobre" style="color: #fff;">Sobre</a>
-        </li>
-      </ul>
+        <nav class="navbar navbar-expand-lg fixed-top navbar-inverse" style="background-color: #38B6FF;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php#">
+                    <img src="images/logo.png" alt="logo" width="100">
+                </a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#conscientizacao" style="color: #fff;">Conscientização</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#dowloads" style="color: #fff;">Downloads</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#sobre" style="color: #fff;">Sobre</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="linha container-fluid py-5">aaaaaaaaa</div>
     </div>
-  </div>
-  
-
-</nav>
-<div class="text-bg-info p-3" style="background: gradient(linear, left top, right top, from(#bd6467), to(#40e0d0)) no-repeat;"></div>
-</div>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
 </body>
 </html>
+
