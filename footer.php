bfba90f8cc303c65d284470ea6032c78030eebd2
carol <adrianycaroline133@gmail.com>
2023-03-17 17:26:08 -0300

    footer

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="Pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="CSS/estilo.css">
+    <title>Footer</title>
+</head>
+<body>
+<div class="fixed-bottom" style="background-color: #1977ad;" >
+<figure>
+<blockquote class="blockquote">
+<div class="d-flex justify-content-around">
+<div>Atendimento <i class="bi bi-headset" style="color:white"></i></div>
+<div>Fale com noix </div>
+<div>Siga-nos <i class="bi bi-hand-thumbs-up" style="color:antiquewhite"></i></div>
+</div>
+</blockquote>
+
+
+<figcaption class="blockquote-footer">
+</figcaption>
+</figure>
+</div>
+</body>
+</html>
