94b813cccaa72515f9ce519ba2ff42b0d7496778
carol <adrianycaroline133@gmail.com>
2023-03-17 17:14:44 -0300

    fazendo o header e add arquivos

--- header_publico.php
+++ header_publico.php
@@ -5,32 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="">
     <link rel="stylesheet" href="CSS/estilo.css">
     <title>Header Publico</title>
 </head>
 <body>
     <div>
-        <nav class="navbar navbar-expand-lg fixed-top navbar-inverse" style="background-color: #38B6FF;">
+        <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #38B6FF;">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php#">
-                    <img src="images/logo.png" alt="logo" width="100">
+                    <img src="images/logo.png" alt="logo" width="150">
                 </a>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php#conscientizacao" style="color: #fff;">Conscientização</a>
+                    <ul class="navbar-nav ms-auto me-auto d-flex gap-5">
+                        <li class="nav-item buttonBoxMenu">
+                            <a class="nav-link" href="index.php#conscientizacao" style="color: #fff; font-size: 20px;">Conscientização</a>
+                        </li>
+                        <li class="nav-item buttonBoxMenu">
+                            <a class="nav-link" href="index.php#dowloads" style="color: #fff; font-size: 20px;">Downloads</a>
+                        </li>
+                        <li class="nav-item buttonBoxMenu">
+                            <a class="nav-link" href="index.php#sobre" style="color: #fff; font-size: 20px;">Sobre</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php#dowloads" style="color: #fff;">Downloads</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php#sobre" style="color: #fff;">Sobre</a>
+                          <a href="" class="nav-link" style="color: #fff;">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                          </svg>
+                          </a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-        <div class="linha container-fluid py-5">aaaaaaaaa</div>
+        <div class="linha container-fluid py-5"></div>
     </div>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
