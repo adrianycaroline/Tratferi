cf53d4aa434fc4583a28b16c6c2bee25347b88ec
carol <adrianycaroline133@gmail.com>
2023-03-17 15:53:26 -0300

    adicionando aqvs e começando o header

--- header_publico.php
+++ header_publico.php
@@ -28,11 +28,7 @@
       </ul>
     </div>
   </div>
-  
-
 </nav>
-<div class="text-bg-info p-3" style="background: gradient(linear, left top, right top, from(#bd6467), to(#40e0d0)) no-repeat;"></div>
-</div>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
