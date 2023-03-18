e79afc287b3fdc3ddc97a5cb3696a4552eb45ffe
carol <adrianycaroline133@gmail.com>
2023-03-17 22:59:06 -0300

    arrumando cards

--- conteudo_home.php
+++ conteudo_home.php
@@ -23,39 +23,51 @@
 <!-- cards -->
 <div class="d-flex gap-5 justify-content-around">
 <div class="card" style="width: 18rem; background-color: #38B6FF;">
-  <div class="card-body">
+<br>
+
+  <div class="card-body d-flex justify-content-around">
     <div>
     <img src="images/MicrosoftTeams-image (8).png" alt="" width="110" class="img-responsive">
     </div>
-    <p class="card-text" style="color: #fff;">Consultas</p>
+    <p class="card-text" style="color: #fff;"><br>Consultas</p>
   </div>
 </div>
 
 <div class="card" style="width: 18rem; background-color: #38B6FF;">
-  <div class="card-body">
+<br>
+  <div class="card-body d-flex justify-content-around">
     <div>
     <img src="images/MicrosoftTeams-image (7).png" alt="" width="110" class="img-responsive">
     </div>
-    <p class="card-text" style="color: #fff;">Agendamentos de Exames</p>
+    <div>
+    <p class="card-text" style="color: #fff;"><br> Agendamentos de Exames</p>
+    </div>
   </div>
 </div>
 
 <div class="card" style="width: 18rem; background-color: #38B6FF;">
-  <div class="card-body">
+<br>
+  <div class="card-body d-flex justify-content-around">
     <div>
     <img src="images/MicrosoftTeams-image (9).png" alt="" width="110" class="img-responsive">
     </div>
-    <p class="card-text" style="color: #fff;">Vacinas</p>
+    <div>
+    <p class="card-text" style="color: #fff;"><br> Vacinas</p>
+    </div>
   </div>
 </div>
 
 <div class="card" style="width: 18rem; background-color: #38B6FF;">
-  <div class="card-body">
-    <div>
-    <img src="images/MicrosoftTeams-image (6).png" alt="" width="110" class="img-responsive">
+    <br>
+    <div class="card-body d-flex justify-content-around">
+        <div>
+            <img src="images/MicrosoftTeams-image (6).png" alt="" width="110" class="img-responsive">
+        </div>
+        <div>
+            <p class="card-text" style="color: #fff;"><br> Resultados de Exames</p>
+        </div>
     </div>
-    <p class="card-text" style="color: #fff;">Resultados de Exames</p>
-  </div>
+    <br>
 </div>
 </div>
 <!-- fim cards -->
