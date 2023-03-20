df01031d722077f6e426be6a2655e31027af70d3
NicolasBraite <NicolasBraite07@gmail.com>
2023-03-20 00:01:58 -0300

    footer feito,inicio login

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
 </svg>
-&nbsp;adrianycaroline133@gmail.com
+&nbsp;tratferi22@gmail.com
 </div>
 <div>
 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
@@ -32,7 +32,7 @@
 &nbsp;(11) 94699-0301
 </div>
 </div>
-<div style="color: white;">Fale com noix </div>
+<div style="color: white;">Fale conosco </div>
 <div style="color: white;">Siga-nos <i class="bi bi-hand-thumbs-up" style="color:antiquewhite"></i></div>
 </div>
 </blockquote>
@@ -41,8 +41,9 @@
 <figcaption class="blockquote-footer">
 </figcaption>
 </figure>
-<div class="text-bg-info p-3" style="background: -webkit-gradient(linear, left top, right top, from(#38B6FF), to(#38B6FF)) no-repeat;"></div>
+<div class="text-bg-info p-3" style="background: -webkit-gradient(linear, left top, right top, from(#38B6FF), to(#38B6FF)) no-repeat;">
+<h6 class="text-center" style="color: white">©2023 Tratferi-Todos os direitos reservados</h6></div>
 </div>
-
+</div>
 </body>
 </html>
--- header_publico.php
+++ header_publico.php
@@ -41,7 +41,7 @@
                     </ul>
                     <div class="navbar-nav px-2">
                         <div class="nav-item buttonBoxMenu" style="background-color: #E88F07;">
-                            <a href="" class="nav-link " style="color: #fff; font-size: 20px;"> Entrar
+                            <a href="login.php" class="nav-link " style="color: #fff; font-size: 20px;"> Entrar
                             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
