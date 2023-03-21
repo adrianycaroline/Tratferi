4007901826791e78e27df5dccae91c5afd3bcd1a
notsumez <adahorapirola@gmail.com>
2023-03-21 14:18:50 -0300

    header enviando para o login

--- admin/login.php
+++ admin/login.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href='CSS/estilo.css'>
+    <link rel="stylesheet" href='../CSS/estilo.css'>
     <title>teste</title>
 </head>
 <body>
