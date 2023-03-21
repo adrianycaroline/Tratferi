c6b96dc353da0229ea0219f6f77a0906dfdea66a
gustavo-lopes10 <gustavodlopes10@gmail.com>
2023-03-21 15:59:26 -0300

    Inicio do back do login_cliente

--- admin/login.php
+++ admin/login.php
@@ -1,12 +1,38 @@
-<php?>
-
-</php>
+<?php 
+    include '../connection/connect.php';
+      // iniciar a verificação do login
+      if($_POST){
+        $email = $_POST['email'];
+        $senha = md5($_POST['senha_usuario']);
+        $loginRes = $conn->query("select * from tbusuarios where login_usuario = '$login' and senha_usuario = '$senha'");
+        $rowLogin = $loginRes->fetch_assoc();
+        $numRow = mysqli_num_rows($loginRes);
+        // se a sessão não existir
+        if(!isset($_SESSION)){
+            $sessaoAntiga = session_name('chulettaaa');
+            session_start();
+            $session_name_new = session_name();
+        }
+        if($numRow>0){
+            $_SESSION['login_usuario'] = $login;
+            $_SESSION['nivel_usuario'] = $rowLogin['nivel_usuario'];
+            $_SESSION['nome_da_sessao'] = session_name();
+            if($rowLogin['nivel_usuario']=='sup'){
+                echo "<script>window.open('index.php','_self')</script>";
+            }else{
+                echo "<script>window.open('../cliente/index.php?cliente=".$login."','_self')</script>";
+            }
+        }else{
+            echo "<script>window.open('invasor.php','_self')</script>";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="pt_br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href='CSS/estilo.css'>
+    <link rel="stylesheet" href='../CSS/estilo.css'>
     <title>teste</title>
 </head>
 <body>
@@ -17,12 +43,12 @@
                     <h2>Login</h2>
                     <div class="inputbox">
                         <ion-icon name="mail-outline"></ion-icon>
-                        <input type="email" required>
+                        <input type="email" name="email" required>
                         <label for="">E-mail</label>
                     </div>
                     <div class="inputbox">
                         <ion-icon name="lock-closed-outline"></ion-icon>
-                        <input type="password" required>
+                        <input type="password" name="password" required>
                         <label for="">Senha</label>
                     </div>
                     <div class="forget">
