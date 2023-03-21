997670e62a844a783b65c8bd5597aeafff3d41dd
gustavo-lopes10 <gustavodlopes10@gmail.com>
2023-03-21 17:00:35 -0300

    Correção de erros

--- admin/login_cliente.php
+++ admin/login_cliente.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href='../CSS/estilo.css'>
-    <title>teste</title>
     <link rel="shortcut icon" href="../images/logo_minimizada.png" type="image/x-icon">
     <title>Login</title>
 </head>
