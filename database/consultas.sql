966818a1b67646967bc413a98d1cd48ff29a7902
Danielsoliveira44 <ds5676419@gmail.com>
2023-03-28 16:09:57 -0300

    Implementando login

--- database/consultas.sql
+++ database/consultas.sql
@@ -1,5 +1,6 @@
 -- ##################### login paciente ##################
-SELECT cpf, adm, ativo FROM funcionario;
+SELECT cpf, adm, ativo, senha FROM funcionario
+inner join login_func where cpf = $cpf and senha = $senha;
 
 -- ######### informaçoes de paciente com consulta marcada ################
 
