a96ae09e6224c2b572a7d755df8707b168ea7a23
Andrey da Hora <adahorapirola@gmail.com>
2023-03-31 23:20:56 -0300

    Página sobre terminada

--- sobre.php
+++ sobre.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="shortcut icon" href="images/logo_minimizada.png" type="image/x-icon">
     <title>Sobre - TRATFERI</title>
 </head>
-<body class="cinza">
+<body>
     <?php include 'header_publico.php';?>
     <main>
         <div style="display: flex;">
@@ -16,11 +17,34 @@
             <div class="container">
                 <br>
                 <h1 class="text-center">Sobre</h1>
+                <div style="border-bottom: 2px solid black; margin-left: 200px; margin-right: 200px;"></div>
                 <br>
-                <p>O TRATFERI, fundado em 2023 pelo grupo de Enfermagem do Senac Itaquera veio ao mundo como uma idéia de</p>
+                <p>O TRATFERI, fundado em 2023 pelo grupo de Enfermagem do Senac Itaquera veio ao mundo como 
+                    uma idéia de aplicação Web e Software que auxiliasse os profissionais de saúde com os 
+                    pacientes, além de ser voltado para adultos que desejam consultar suas informações
+                    e progresso do atendimento. <br>
+                    Assim, o TRATFERI (TRATamento de FERIdas) foi oficialmente produzido. Atendendo as
+                    melhores funcionalidades esse sistema é de grande ajuda a profissionais que precisam
+                    de respostas rápidas para certos atendimentos e até oferece um auxílio de ponta na
+                    organização e no atendimento.</p>
             </div>
         </div>
     </main>
     <?php include 'footer.php';?>
 </body>
+<!-- link para bootstrap -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min-js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).on('ready',function(){
+        $(".regular").slick({
+            dots: true,
+            infinity: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });
+    });
+</script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick/slick.min.js"></script>
 </html>
