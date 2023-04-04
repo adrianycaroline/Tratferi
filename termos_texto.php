6972e6eb0bea00e546c54054542418b45d99a122
adrianycaroline <adrianycaroline133@gmail.com>
2023-04-04 15:57:26 -0300

    add links

--- termos_texto.php
+++ termos_texto.php
@@ -39,4 +39,6 @@
     </div>
     
 </body>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </html>
