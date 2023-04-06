b8b065c014d99066f9db396e6efb8956565c8e28
Andrey da Hora <adahorapirola@gmail.com>
2023-04-05 23:58:01 -0300

    adição de consultas para testes

--- database/consultas.sql
+++ database/consultas.sql
@@ -10,7 +10,21 @@ inner join login_paci where cpf = $cpf and senha = $senha;
 
 
 
--- teste para login 
+-- teste para login funcionario
 
 insert into funcionario
-VALUES('4','daniel','1998/06/05','257.987.099-97','000','000','98.795.521-7','m','responsavel pela manutençao e bom funcionamento do sistema','noite','10001.00','2017/01/09','2017/01/10',1,1);
+VALUES('1','daniel','2004/10/07','145.326.268-23','102.658','596.987','90.156.984-8','enfermeiro','realizar triagens, preencher prontuários, oferecer os primeiros atendimentos, coletar exames, administrar medicamentos e monitorar o estado de saúde dos pacientes.','noite','3300,00','2020/02/01','null','func','0');
+
+insert into funcionario
+VALUES('2','andrey','2000/03/10','359.742.031-98','032.987','158.387','88.032.603-7','auxiliar enfermagem','realiza a coleta de sangue para exames, faz curativos simples, administra remédios, ajuda na higiene dos pacientes, entre outras funções.','manha','2429,00','2020/02/01','null','adm','1');
+
+select * from funcionario;
+
+
+Insert into Login_func
+Values('1','1234','1');
+
+Insert into Login_func
+Values('2','1234','2');
+
+select * from Login_func;
