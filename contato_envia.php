8bf39a2e5edc1dbdce473218f63eeeede8e302b6
gustavo-lopes10 <gustavodlopes10@gmail.com>
2023-04-13 12:18:38 -0300

    Inclusão de imagem em contato_envia.php

--- contato_envia.php
+++ contato_envia.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/logo_minimizada.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/estilo.css">
     <title>Contato</title>
 </head>
-    <body class="fundo_cont">
+    <body>
         <?php
         $nome = $_POST['nome_contato'];
         $email = $_POST['email_contato'];
@@ -47,6 +48,8 @@ require './lib/vendor/autoload.php';
             <!-- se não funcionar  -->
             <h3 class="texto_contato">E-mail não enviado com sucesso. Tente novamente</h3>
         <?php }?>
-        
+            <div class="img_contato">
+                <img src="images/fundo_contato.png" alt="">
+            </div>
     </body>
 </html>
--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                           <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                         </svg>
-                        <a class="ancora" href="contato.php">&nbsp;tratferi@gmail.com</a>
+                        &nbsp;tratferi@gmail.com
                       </div>
                       <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
@@ -51,7 +51,7 @@
                               <a class="nav-link" href="sobre.php" style="color: #fff; font-size: 20px;">Sobre</a>
                             </tr>
                             <tr class="nav-item">
-                              <a class="nav-link" href="" style="color: #fff; font-size: 20px;">Fale conosco</a>
+                              <a class="nav-link " href="contato.php" style="color: #fff; font-size: 20px;">Fale conosco</a>
                             </tr>
                           </table>
                         </div>
