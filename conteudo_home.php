106ac10fdfa2810b4aa6f1a325969dc0ea26cd6e
Andrey da Hora <adahorapirola@gmail.com>
2023-04-12 21:02:00 -0300

    organizando a página conteúdo home

--- conteudo_home.php
+++ conteudo_home.php
@@ -7,110 +7,112 @@
     <title>Conteudo Home</title>
 </head>
 <body>
-    <!-- banner -->
-    <div class="position-relative">
-      <img src="images/bann.png" class="img-responsive" width="100%">
-      <!-- barra de pesquisa -->
+  <!-- banner -->
+  <div class="position-relative">
+    <img src="images/bann.png" class="img-responsive" width="100%">
+    <!-- barra de pesquisa -->
     <nav class="navbar bg-body-tertiary position-absolute top-50 start-50 translate-middle">
       <div class="container-fluid">
         <form class="d-flex" role="search">
           <div class="embaixo">
-          <img src="images/logoPesq.png" class="img-responsive" width="250">
-        <div class="flex">
-          <input class="pesquisar" type="search" placeholder="O que você procura?" aria-label="Search" width="100%">
-          <button class="border" type="submit" style="background-color: #38B6FF;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="35" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16" style="color: #fff;">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-          </button>
+            <img src="images/logoPesq.png" class="img-responsive" width="250">
+            <div class="flex">
+              <input class="pesquisar" type="search" placeholder="O que você procura?" aria-label="Search" width="100%">
+              <button class="border" type="submit" style="background-color: #38B6FF;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="35" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16" style="color: #fff;">
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </nav>
+  </div> <!-- fim barra de pesquisa --><!-- fim do banner -->
+  <div style="background-color: #fff;">
+    <br>
+    <br>
+    <!-- começo dos cards  -->
+    <div class="d-flex gap-5 justify-content-around">
+      <div class="card" style="width: 18rem;" >
+        <img src="images/atendimento.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">
+            Nossos ideais como empresa 
+            <br><br>  
+            é pensar na acessibilidade entre
+            Paciente e cliente dessa forma colocando você e sua saúde em
+            Primeiro lugar sempre esse é nosso objetivo como uma marca.
+          </p>
         </div>
       </div>
-      </form>
+      <div class="card" style="width: 18rem;">
+        <img src="images/anota.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text"> 
+            Como Entregamos nosso atendimento ?
+            <br><br> 
+              dentro da sua casa.<br>
+              Nossos profissionais acompanharão o seu tratamento<br>
+              Pensando na melhor forma pra você sentir-se bem.<br>
+          </p>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="images/pc.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Porque confiar no tratferi ?
+            <br><br>
+            Precisamos de informações basicas sobre o paciente<br>
+            Para atender da melhor forma possivel apenas isso,<br>
+            Nosso plano é a sua saúde.
+          </p>
+        </div>
+      </div>
     </div>
-  </nav>
-  <!-- fim barra de pesquisa -->
-  </div>
-    <!-- fim do banner -->
-    <div style="background-color: #b2f8f9;">
     <br>
     <br>
-<!-- começo dos cards  -->
-<!-- ------------------------------------------ -->
-
-<div class="d-flex gap-5 justify-content-around">
-<div class="card" style="width: 18rem;">
-  <img src="images/atendimento.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Nossos ideais como empresa 
-    <br> <br>  
-    é pensar na acessibilidade entre
-Paciente e cliente dessa forma colocando você e sua saúde em
-Primeiro lugar sempre esse é nosso objetivo como uma marca.</p>
+    <div class="d-flex gap-5 justify-content-around">
+      <div class="card" style="width: 18rem;" >
+        <img src="images/atendimento.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">
+            Nossos ideais como empresa 
+            <br><br>  
+            é pensar na acessibilidade entre
+            Paciente e cliente dessa forma colocando você e sua saúde em
+            Primeiro lugar sempre esse é nosso objetivo como uma marca.
+          </p>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="images/anota.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text"> 
+            Como Entregamos nosso atendimento ?
+            <br><br> 
+              dentro da sua casa.<br>
+              Nossos profissionais acompanharão o seu tratamento<br>
+              Pensando na melhor forma pra você sentir-se bem.<br>
+          </p>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="images/pc.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Porque confiar no tratferi ?
+            <br><br>
+            Precisamos de informações basicas sobre o paciente<br>
+            Para atender da melhor forma possivel apenas isso,<br>
+            Nosso plano é a sua saúde.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- fim cards -->
+    <br>
+    <br>
+    <br>
   </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="images/anota.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text"> Como Entregamos nosso atendimento ?
-      <br> <br> 
-dentro da sua casa.<br>
-Nossos profissionais acompanharão o seu tratamento<br>
-Pensando na melhor forma pra você sentir-se bem.<br>
-</p>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="images/pc.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Porque confiar no tratferi ?
-<br><br>
-Precisamos de informações basicas sobre o paciente<br>
-Para atender da melhor forma possivel apenas isso,<br>
-Nosso plano é a sua saúde.</p>
-  </div>
-</div>
-</div>
-<!-- ------------------------------------------ -->
-<br>
-<br>
-<div class="d-flex gap-5 justify-content-around">
-<div class="card" style="width: 18rem;">
-  <img src="images/atendimento.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Nossos ideais como empresa 
-    <br> <br>  
-    é pensar na acessibilidade entre
-Paciente e cliente dessa forma colocando você e sua saúde em
-Primeiro lugar sempre esse é nosso objetivo como uma marca.</p>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="images/anota.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text"> Como Entregamos nosso atendimento ?
-      <br> <br> 
-dentro da sua casa.<br>
-Nossos profissionais acompanharão o seu tratamento<br>
-Pensando na melhor forma pra você sentir-se bem.<br>
-</p>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="images/pc.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Porque confiar no tratferi ?
-<br><br>
-Precisamos de informações basicas sobre o paciente<br>
-Para atender da melhor forma possivel apenas isso,<br>
-Nosso plano é a sua saúde.</p>
-  </div>
-</div>
-</div>
-
-<!-- fim cards -->
-<br>
-<br>
-<br>
-</div>
 </body>
 </html>
