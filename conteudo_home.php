310e0dbcae820d385ff62d4d8aa5282e7115bf4a
adrianycaroline <adrianycaroline133@gmail.com>
2023-04-13 16:09:16 -0300

    add meta

--- conteudo_home.php
+++ conteudo_home.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" href="images/logo_minimizada.png" type="image/x-icon">
     <title>Conteudo Home</title>
 </head>
