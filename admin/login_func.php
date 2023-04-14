49d218939735cdbc5cbb948309b52d7c6ecfc2d7
carol <adrianycaroline133@gmail.com>
2023-04-14 18:42:34 -0300

    .

--- admin/login_func.php
+++ admin/login_func.php
@@ -32,6 +32,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href='../CSS/estilo.css'>
     <link rel="shortcut icon" href="../images/logo_minimizada.png" type="image/x-icon">
     <title>Login Funcionario</title>
