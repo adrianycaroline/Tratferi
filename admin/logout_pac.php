a928f323b0ae85052516a290f8c1fec071e11083
Andrey da Hora <adahorapirola@gmail.com>
2023-04-13 22:57:36 -0300

    contador de horas funcionando na area do funcionario

--- admin/logout_pac.php
+++ admin/logout_pac.php
@@ -2,6 +2,6 @@
     session_name('tratferi');
     session_start();
     session_destroy();
-    header('location: ../index.php');
+    header('location: ../admin/login_cliente.php');
     exit;
 ?>
--- client/paci_options.php
+++ client/paci_options.php
@@ -31,9 +31,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/estilo.css">
+    <script src="../js/script.js"></script>
     <title>Área do Funcionário</title>
 </head>
-<body>
+<body onload="atualizarHoras()">
 <div>
     <main id="escondido2"  style="bottom: 0;">
         <div class="bg-azul"> 
