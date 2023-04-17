745dec35b71f83a98530980650f91aed63724d8c
gustavo-lopes10 <gustavodlopes10@gmail.com>
2023-04-17 12:27:02 -0300

    redirecionamento em alguns segundos se nada for feito

--- admin/cadastro.php
+++ admin/cadastro.php
@@ -25,6 +25,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="50;URL= ../index.php">
     <link rel="stylesheet" href='../CSS/estilo.css'>
     <link rel="shortcut icon" href="../images/logo_minimizada.png" type="image/x-icon">
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
--- admin/login_cliente.php
+++ admin/login_cliente.php
@@ -26,6 +26,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="30;URL= ../index.php">
     <link rel="stylesheet" href='../CSS/estilo.css'>
     <link rel="shortcut icon" href="../images/logo_minimizada.png" type="image/x-icon">
 
