2d433853d8abd21471baf5e97ceb06aa6c84f78b
ANDREY DA HORA PIROLA <andrey.hpirola@senacsp.edu.br>
2023-04-20 14:31:57 -0300

    Listando e exlcuindo funcionario

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -88,7 +88,7 @@
             </div>
         </div>
     </div>
-    <?php include '../admin/adm_options.php';?>
+    
 </body>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
