66906755984b541653ca3ace8f8f748ce0920f56
carol <adrianycaroline133@gmail.com>
2023-04-24 20:12:32 -0300

    fazendo parte do contato atendimento no footer

--- footer.php
+++ footer.php
@@ -23,13 +23,18 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                           <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                         </svg>
-                        &nbsp;tratferi@gmail.com
+                        <a href="mailto:tratferi_site@outlook.com?subject=Contato do Site" style="color: #fff;">
+                        &nbsp;tratferi_site@outlook.com
+                        </a>
                       </div>
+                      <br>
                       <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
                           <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                         </svg>
+                        <a href="https://api.whatsapp.com/send?phone=5511946990301&text=Ol%C3%A1" style="color: #fff;">
                         &nbsp;(11) 94699-0301
+                        </a>
                       </div>
                     </div>
                       <div style="color: white;">
