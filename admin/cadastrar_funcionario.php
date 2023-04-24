e82f02ac7d8d027fa34bd9ce9c902dc283b6f32f
adrianycaroline <adrianycaroline133@gmail.com>
2023-04-24 16:48:46 -0300

    terminando cadastro, arrumando img do card do home

--- admin/cadastrar_funcionario.php
+++ admin/cadastrar_funcionario.php
@@ -40,7 +40,97 @@
     <link rel="stylesheet" href="../CSS/cadastro.css">
     <title>Cadastrar Funcionário</title>
 </head>
+<!-- ---------------------------------------------------------------------------- -->
+<div>
+    <div class="fixed" style="background-color: #38B6FF;" >
+        <br> <br>
+    </div>
+        <div class="d-flex flex-column flex-shrink-0 p-3 " style="position: fixed; top: 0; left: 0; bottom: 0; width: 280px; background-color: #38B6FF;">
+            <div>
+                <figure style="display: flex;">
+                    <img src="../images/logoPesq.png" alt="Logo Tratferi" width="75vw"> 
+                    <a href="../index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none" style="font-size: 32pt;">
+                        Tratferi
+                    </a> 
+                </figure>
+            </div>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li>
+                    <a href="../admin/index.php" class="nav-link" aria-current="page" style="color: #fff;">
+                        <img src="../images/dashboard.svg" alt="" width="20vw">
+                        Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="../admin/cadastrar_funcionario.php" class="nav-link" style="color: #fff;">
+                        <img src="../images/usuarios.svg" alt="" width="20vw">
+                        Cadastrar
+                    </a>
+                </li>
+                <li>
+                    <a href="funcionarios_arquivados.php" class="nav-link" style="color: #fff;">
+                        <img src="../images/usuarios.svg" alt="" width="20vw">
+                        Usuarios Arquivados
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link" style="color: #fff;">
+                        <img src="../images/cama.svg" alt="" width="20vw">
+                        Leitos
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link" style="color: #fff;">
+                        <img src="../images/predio.svg" alt="" width="20vw">    
+                        Departamentos
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link" style="color: #fff;">
+                        <img src="../images/predio.svg" alt="" width="20vw">    
+                        Designação
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link" style="color: #fff;">
+                        <img src="../images/predio.svg" alt="" width="20vw">    
+                        RH
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link" style="color: #fff;">
+                        <img src="../images/coracao.svg" alt="" width="20vw">    
+                        Ajuda
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link" style="color: #fff;">
+                        <img src="../images/anuncio.svg" alt="" width="20vw">
+                        Anuncios
+                    </a>
+                </li>
+            </ul>
+            <hr>
+            <div class="dropdown">
+                <img src="../fotos_usuarios/<?php echo $_SESSION['Imagem']; ?>" alt="Foto de Perfil - <?php echo $_SESSION['nome']?>" width="32" height="32" class="rounded-circle me-2">
+                <strong><a style="text-decoration: none; color: white;"><?php echo $_SESSION['nome'];?></a></strong>
+                <button class="dropdown-toggle border-0" style="background-color: #38B6FF; color: white;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                </button>
+                <div class="dropdown-menu user" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="../admin/perfil/config_perfil.php"><ion-icon name="person-outline"></ion-icon>Perfil</a>
+                    <a class="dropdown-item" href="../admin/logout_Fun.php"><ion-icon name="exit-outline"></ion-icon>Sair</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ------------------------- -->
 <body class="fundo_areas">
+                <figure style="display: flex;">
+                    <img src="../images/logoPesq2.png" alt="Logo Tratferi" width="75vw"> 
+                    <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none" style="font-size: 32pt; color: #38B6FF;">
+                        Tratferi
+                    </a> 
+                </figure>
     <div class="container mt-4 d-flex justify-content-center">
         <div class="card" style="width: 50%;">
             <h1 class="card-header text-center" style="color: #38B6FF;">Cadastro de Funcionário</h1>
