812e9b1eddcbe5657a8f77a90c9b348ff2095772
Andrey da Hora <adahorapirola@gmail.com>
2023-04-25 18:57:05 -0300

    mudanças

--- admin/index.php
+++ admin/index.php
@@ -17,9 +17,12 @@
     <?php include 'menu_adm.php'; ?>
     <?php include '../admin/adm_options.php';?>
 </body>
+<!-- Links para a Biblioteca de icones do Ionic Icons -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <!-- link para bootstrap -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
+<script src="../../js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-2.2.0.min-js" type="text/javascript"></script>
 <script type="text/javascript">
     $(document).on('ready',function(){
@@ -32,5 +35,21 @@
     });
 </script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick/slick.min.js"></script>
+<script type="text/javascript">
+    $('.delete').on('click',function(){
+        var nome = $(this).data('nome'); //busca o nome com a descrição (data-nome)
+        var id = $(this).data('id'); // busca o id (data-id)
+        //console.log(id + ' - ' + nome); //exibe no console
+        $('span.nome').text(nome); // insere o nome do item na confirmação
+        $('a.delete-yes').attr('href','usuario_excluir.php?id='+id); //chama o arquivo php para excluir o produto
+        $('#modalEdit').modal('show'); // chamar o modal
+    });
+</script>
+<!-- Links para jquery -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
 </html>
 
