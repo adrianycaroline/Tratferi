bd9acf230f3c10a7118cc78400d7fc91d5c188a3
adrianycaroline <adrianycaroline133@gmail.com>
2023-04-26 16:54:25 -0300

    front-end funcionario e cadastrar paciente

--- func/cadastrar_paciente.php
+++ func/cadastrar_paciente.php
@@ -7,7 +7,7 @@
         // $now_format = $data->format('Y-m-d');
         $cpf = $_POST['cpf'];
         $rg = $_POST['rg'];
-        $sus = $_POST['card_sus'];
+        $sus = $_POST['card_SUS'];
         $hashcode = "TRAT-".uniqid()."-".mb_strimwidth($cpf,0,3);
         if($sus == ""){
         $loginRes = $conn->query("insert into paciente (nome, data_nasc, cpf, rg, card_SUS, imagem, hash) values('$nome', '$data', '$cpf', '$rg', 'null', 'null', '$hashcode')");
@@ -35,46 +35,58 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <title>Cadastro</title>
 </head>
-<body>
-    <section id="fundo_cli" style="display: flex; flex-direction: column;">
-        <?php include '../logo_superior.php'?>
-        <div id="formbox-cadastro" class="sombra">
-            <div class="form-value">
-                <form action="cadastrar_paciente.php" method="post">
-                    <h2>Cadastrar Paciente</h2>
-                    <div class="inputbox">
-                        <ion-icon name="person-outline"></ion-icon>
-                        <input type="text" name="nome" required>
-                        <label for="">Nome</label>
+<body class="fundo_areas">
+<div class="barra fixed-top" style="background-color: #38B6FF;">
+        <br> <br>
+ </div>
+ <br><br>
+<div class="container mt-4 d-flex justify-content-center">
+        <div class="card" style="width: 50%;">
+            <h1 class="card-header text-center" style="color: #38B6FF;">Cadastro de Paciente</h1>
+            <div class="card-body">
+                <!-- Formulário de Cadastro de Funcionário -->
+                <form class="container" style="display: flex; justify-content: flex-start; flex-direction: column;" action="cadastrar_funcionario.php" method="post" name="form_funcionario_cadastro" enctype="multipart/form-data" onsubmit="return validaForm() && validaFormPeriodo()">
+                    <div class="form-row" style="display: flex; justify-content: center; flex-direction: column;">
+                        <div class="form-group">
+                            <label for="nome">Nome Completo</label>
+                            <input type="text" name="nome" maxlength="50" class="form-control" id="nome" placeholder="Digite o nome do paciente completo " required>
+                        </div>
+                        <br>
+                        <div class="form-group ">
+                            <label for="data_nasc">Data de Nascimento</label>
+                            <input type="date" name="data" class="form-control" id="data_nasc" min="1940-01-01" max="<?php echo date('Y-m-d', strtotime('-18 years')); ?>" required>
+                        </div>
                     </div>
-                    <div class="inputbox">
-                        <ion-icon name="calendar"></ion-icon>
-                        <input type="date" name="data_nasc" required>
-                        <label for="">Data de Nascimento</label>
+                    <br>
+                    <div class="form-row">
+                        <div class="form-group ">
+                            <label for="cpf">CPF</label>
+                            <input type="text" name="cpf" class="form-control" id="cpf" placeholder="Digite o CPF do paciente" onkeypress="$(this).mask('000.000.000-00');" required>
+                        </div>
+                        <br>
                     </div>
-                    <div class="inputbox">
-                        <ion-icon name="wallet-outline"></ion-icon>
-                        <input type="text" name="cpf" onkeypress="$(this).mask('000.000.000-00');" required>
-                        <label for="">CPF</label>
+                    <div class="form-row">
+                        <div class="form-group ">
+                            <label for="rg">RG</label>
+                            <input type="text" name="rg" class="form-control" id="cpf" placeholder="Digite o RG do paciente" onkeypress="$(this).mask('00.000.000-0');" required>
+                        </div>
+                        <br>
                     </div>
-                    <div class="inputbox">
-                        <ion-icon name="wallet-outline"></ion-icon>
-                        <input type="text" name="rg" onkeypress="$(this).mask('00.000.000-0');" required>
-                        <label for="">RG</label>
+                    <div class="form-row">
+                        <div class="form-group ">
+                            <label for="card_sus">Cartão do SUS</label>
+                            <input type="text" name="cart_sus" class="form-control" id="cpf" placeholder="Digite o cartão do SUS do paciente" onkeypress="$(this).mask('00.000.000-0');" required>
+                        </div>
+                        <br>
                     </div>
-                    <div class="inputbox">
-                        <ion-icon name="wallet-outline"></ion-icon>
-                        <input type="text" name="card_sus">
-                        <label for="">Cartão SUS</label>
-                    </div>
-                    <button type="submit" id="loginBtn">Prosseguir</button>
-                    <div class="register">
-                    </div>
+                    <br>
+                    <button class="btn" type="submit" style="background-color: #38B6FF;">Cadastrar</button>
                 </form>
             </div>
         </div>
-        <?php include '../termos_texto.php';?>
-    </section>
+    </div>
+    <!-- --------------------- -->
+        <?php include 'menu_cadastrar.php';?>
 </body>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
