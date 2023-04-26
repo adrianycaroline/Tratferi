d04c1f3218ae4cf090eeedf9ee3befc1012b4a0b
NicolasBraite <NicolasBraite07@gmail.com>
2023-04-26 11:54:07 -0300

    começo ajuda

---
+++ ajuda.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="Pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/logo_minimizada.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="CSS/estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Central de Ajuda</title>
+<!-- background ajuda -->
+<style>
+body {
+        background: url('images/fundo_ajuda.jpeg') no-repeat;  
+        background-size: cover;
+    }
+</style>
+<body>
+
+</body>
+<!-- fim background ajuda -->
