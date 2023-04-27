6c43462a89ebaecc9c2a91290ff3f8443c1f6844
NicolasBraite <nicolasbraite07@gmail.com>
2023-04-27 15:41:45 -0300

    ajuda terminando

--- ajuda.php
+++ ajuda.php
@@ -11,10 +11,16 @@
 <body>
 <style>
 body {
-        background: url('images/fundo_ajuda.jpeg') no-repeat;  
+        background: url('images/fundo_ajuda.png') no-repeat;  
         background-size: cover;
     }
 </style>
+<!-- Botão voltar -->
+<a href="index.php#">
+  <img src="images/botão_volta.svg" style="width: 50px; margin-left: 4%; margin-top: 2%;">
+</a>
+<!-- fim Botão voltar -->
+
 <h1 style=" display:flex; justify-content: center; color:white; margin-top:4%;">Como podemos ajudar ?</h1>
 <br><br><br>
 <!-- Primeiro Dropdown-->
@@ -35,10 +41,29 @@ body {
       </div>
     </div>
   </div>
+
   <br>
   
-
-<!-- Java sript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <!-- fim primeiro Dropdown,Começo 2  -->
+  <div class="accordion" id="accordionExample1">
+  <div class="accordion-item" style="border: 0;">
+    <h2 class="accordion-header">
+    <button class="accordion-button btn-orange" type="button" data-bs-toggle="collapse" data-bs-target="#collapse" aria-expanded="true" aria-controls="collapse" >
+      Como funciona as consultas presencias ? 
+      </button>
+    </h2>
+    <div id="collapse" class="accordion-collapse collapse show" data-bs-parent="#accordionExample1" style="color:white">
+      <div class="accordion-body" style="color:white; background-color:#01a1ff;">
+        <strong>O objetivo do site é acompanhar o paciente <br>
+         a distancia  até a consulta presencial <br>
+         propriamente dita levando isso em <br>
+         conta ce necessario faremos uma <br>
+         consulta presencial para o paciente.</strong>    
+      </div>
+    </div>
+  </div>
+  
 </body>
 </html>
+<!-- Java sript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
