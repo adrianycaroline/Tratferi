4359028a5b40f4aaf214a3e6be57ce5ff022c737
Danielsoliveira44 <ds5676419@gmail.com>
2023-04-28 14:51:52 -0300

    botao perfil pronto

--- client/menu_paci.php
+++ client/menu_paci.php
@@ -12,7 +12,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <title>Barra Lateral</title>
 </head>
@@ -86,6 +85,9 @@
             </div>
         </div>
     </div>
+    
 </body>
+    
     <script src="../js/script.js"></script>
-</html>
+    
+    </html>
