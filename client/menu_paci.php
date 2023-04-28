799f198d662becf5a4cbeadcb546515b004d6625
fAlmeida99 <felipealalmeida10@gmail.com>
2023-04-28 14:51:43 -0300

    mood

--- client/menu_paci.php
+++ client/menu_paci.php
@@ -12,8 +12,8 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    
     <title>Barra Lateral</title>
 </head>
 <body>
