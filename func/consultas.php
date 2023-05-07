461ac942e466060611f7dc41bff7898e079fd3f8
gustavo-lopes10 <gustavodlopes10@gmail.com>
2023-05-07 15:06:48 -0300

    consultas.php

--- func/consultas.php
+++ func/consultas.php
@@ -7,7 +7,7 @@ $row = $lista->fetch_assoc();
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../images/logo_minimizada.png" type="image/x-icon">
