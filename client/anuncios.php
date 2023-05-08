0f4b7ecdafe8488872cfbc17b00be16ea07fc7a3
NicolasBraite <nicolasbraite07@gmail.com>
2023-05-08 16:37:48 -0300

    anuncios

--- client/anuncios.php
+++ client/anuncios.php
@@ -52,25 +52,31 @@
             </div>
         </div>
     </div>
-    
+    <!-- card 1  -->
     <div class="container" style="display: flex; flex-wrap: wrap; flex-direction: column;">
-        <h1 class="mt-5 mb-4">Anúncios</h1>
+        <h1 class="mt-5 mb-4 " style="color:#1d5f96; display:flex; justify-content:center;">Anúncios</h1>
+        <div style="border-bottom: 2px solid #000; width: 100%; margin: 0 auto;"></div>
+        <br><br><br>
         <div class="row">
             <div class="col-md-4 mb-4">
                 <div class="card h-100">
-                    <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Anúncio+1" alt="Anúncio 1">
+                    <img class="card-img-top" src="../images/aplicativo.png" alt="Anúncio 1">
                     <div class="card-body">
-                        <h5 class="card-title">Anúncio 1</h5>
-                        <p class="card-text">Descrição do anúncio 1.</p>
-                        <a href="#" class="btn btn-primary">Mais detalhes</a>
+                        <h3 class="card-title" style="color:#1d5f96;">Em breve lançamento do Nosso aplicativo...</h3>
+                        <br><br>
+                        <a href="https://play.google.com/store/games?hl=pt_BR&gl=US" class="btn btn-primary">Mais informações android...</a>
+                        <br><br>
+                        <a href=https://www.apple.com/br/app-store/ class="btn btn-primary">Mais informações ios...</a>
                     </div>
                 </div>
             </div>
+            
+            <!-- card 2  -->
             <div class="col-md-4 mb-4">
                 <div class="card h-100">
-                    <img class="card-img-top" src="https://via.placeholder.com/300x200.png?text=Anúncio+2" alt="Anúncio 2">
+                    <img class="card-img-top" src="../images/redesocial.jpg"   alt="Anúncio 2">
                     <div class="card-body">
-                        <h5 class="card-title">Anúncio 2</h5>
+                        <h5 class="card-title">fngibnuibgbniubygskbgdsfhujg</h5>
                         <p class="card-text">Descrição do anúncio 2.</p>
                         <a href="#" class="btn btn-primary">Mais detalhes</a>
                     </div>
