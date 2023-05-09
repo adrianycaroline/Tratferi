3c84a1c2ed56ab1412e60de5b9d95b2af4447e87
Andrey da Hora <adahorapirola@gmail.com>
2023-05-09 00:03:29 -0300

    required no form tratamento

--- func/tratamento.php
+++ func/tratamento.php
@@ -81,9 +81,9 @@
         $insert_tratamento_result = $conn->query($insert_tratamento);
 
         if($insert_tratamento_result && $insert_ferida_result){
-            header('location: index.php?tratamento=s');
+            header('location: listar_tratamento.php?tratamento=s');
         }else{
-            header('location: index.php?tratamento=n');
+            header('location: listar_tratamento.php?tratamento=n');
         }
 
     }
@@ -126,28 +126,28 @@
                         <div class="d-flex" style="justify-content: center;">
                             <div style="margin-right: 70px;">
                                 <h5 style="color: #2b8af7;">Odor:</h5>
-                                <textarea class="TxaTratamento" name="odor" id="odor" cols="40" rows="4"></textarea>
+                                <textarea class="TxaTratamento" name="odor" id="odor" cols="40" rows="4" required></textarea>
                                 <h5 style="color: #2b8af7;">Lateralidade:</h5>
-                                <textarea class="TxaTratamento" name="lateralidade" id="lateralidade" cols="40" rows="4"></textarea>
+                                <textarea class="TxaTratamento" name="lateralidade" id="lateralidade" cols="40" rows="4" required></textarea>
                                 <h5 style="color: #2b8af7;">Medidas:</h5>
-                                <textarea class="TxaTratamento" name="medidas" id="medidas" cols="40" rows="4"></textarea>
+                                <textarea class="TxaTratamento" name="medidas" id="medidas" cols="40" rows="4" required></textarea>
                                 <h5 style="color: #2b8af7;">Descrição da Região afetada:</h5>
-                                <textarea class="TxaTratamento" name="desc" id="desc" cols="40" rows="4"></textarea>
+                                <textarea class="TxaTratamento" name="desc" id="desc" cols="40" rows="4" required></textarea>
                                 <h5 style="color: #2b8af7;">Previsão de Finalização</h5>
-                                <textarea class="TxaTratamento" name="finalizacao" id="finalizacao" cols="40" rows="4"></textarea>
+                                <textarea class="TxaTratamento" name="finalizacao" id="finalizacao" cols="40" rows="4" required></textarea>
                             </div>
                             <div>
                                 <!-- Text Adiquirida -->
                                 <h5 style="color: #2b8af7;">Forma Adquirida:</h5>
-                                <textarea class="TxaTratamento" name="forma" id="forma" cols="40" rows="4"></textarea>
+                                <textarea class="TxaTratamento" name="forma" id="forma" cols="40" rows="4" required></textarea>
                                 <h5 style="color: #2b8af7;">Acompanhamento</h5>
-                                <textarea class="TxaTratamento" name="acompanhamento" id="acompanhamento" cols="40" rows="4"></textarea>
+                                <textarea class="TxaTratamento" name="acompanhamento" id="acompanhamento" cols="40" rows="4" required></textarea>
                                 <div>
                                     <div class="d-flex">
                                         <div>
                                             <!-- Text Medicação a se utilizar -->
                                             <h5>Medicação:</h5>
-                                            <select name="medicamento" id="medicamento" style="font-size: 14pt; border-radius: 10px;">
+                                            <select name="medicamento" id="medicamento" style="font-size: 14pt; border-radius: 10px;" required>
                                                 <?php 
                                                     //seleciona os medicamentos 
                                                     $medicamentos = $conn->query("SELECT * FROM medicamento;");
@@ -160,7 +160,7 @@
                                             <br><br>
                                             <!-- Paciente -->
                                             <h5>Paciente:</h5>
-                                            <select name="paciente" style="font-size: 14pt; border-radius: 10px;">
+                                            <select name="paciente" style="font-size: 14pt; border-radius: 10px;" required>
                                                 <?php 
                                                     while($row = $lista->fetch_assoc()) {
                                                         echo '<option value="'.$row['nome'].'">'.$row['nome'].'</option>';
@@ -171,10 +171,10 @@
                                         <div style="font-size: 14pt; margin-left: 15px;">
                                             <h5>Lado do Corpo:</h5>
                                             <label>
-                                                <input value="lado" type="radio" name="lado" id="lado" value="Frente"> Frente
+                                                <input value="lado" type="radio" name="lado" id="lado" value="Frente" required> Frente
                                             </label>
                                             <label>
-                                                <input value="lado" type="radio" name="lado" id="lado" value="Verso"> Costas
+                                                <input value="lado" type="radio" name="lado" id="lado" value="Verso" required> Costas
                                             </label>
                                         </div>
                                     </div>
@@ -182,12 +182,12 @@
                                     <div>
                                         <div>
                                             <h5>Dia da Lesão</h5>
-                                            <input type="date" name="tempo" id="tempo" style="border-radius: 10px;">
+                                            <input type="date" name="tempo" id="tempo" style="border-radius: 10px;" required>
                                         </div>
                                         <br>
                                         <div>
                                             <h5>Tipo de Membro:</h5>
-                                            <select name="membro" style="font-size: 14pt; border-radius: 10px;">
+                                            <select name="membro" style="font-size: 14pt; border-radius: 10px;" required>
                                                 <option value="superior">Superior</option>
                                                 <option value="inferior">Inferior</option>
                                             </select>
@@ -195,7 +195,7 @@
                                         <br>
                                         <div>
                                             <h5>Exsudato</h5>
-                                            <select name="exsudato" id="exsudato" style="font-size: 14pt; border-radius: 10px;">
+                                            <select name="exsudato" id="exsudato" style="font-size: 14pt; border-radius: 10px;" required>
                                                 <option value="alto">Alto</option>
                                                 <option value="baixo">Baixo</option>
                                             </select>
@@ -211,14 +211,14 @@
                                 <div id="area">
                                     Area: <br>
                                     <span id="data"></span>
-                                    <input type="hidden" name="area" id="area_input">
+                                    <input type="hidden" name="area" id="area_input" required>
                                 </div>
                             </div>
                         </div>
                         <br>
                         <div class="text-center">
                             <h2>Anotações</h2>
-                            <textarea class="TxaTratamento" name="anotacao" id="anotacao" cols="150" rows="10"></textarea>
+                            <textarea class="TxaTratamento" name="anotacao" id="anotacao" cols="150" rows="10" required></textarea>
                             <br><br>
                             <button class="btn btn-primary" type="submit" name="concluir">Criar</button>
                         </div>
